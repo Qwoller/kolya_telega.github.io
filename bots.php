448557a63c59f3a6f8fd95dc58fec17b1daef412
Qwoller <52505198+Qwoller@users.noreply.github.com>
2022-05-19 21:46:05 +0500

    Update bots.php

--- bots.php
+++ bots.php
@@ -1,64 +1,93 @@
-/**
-*   Very simple chat bot @verysimple_bot by Novelsite.ru
-*   05.07.2021
-*/
-header('Content-Type: text/html; charset=utf-8'); // на всякий случай досообщим PHP, что все в кодировке UTF-8
+<?php
 
-$site_dir = dirname(dirname(__FILE__)).'/'; // корень сайта
-$bot_token = '5358046782:AAGiDv_HBq7TcwgWAHFfl_FgHlVJLP0myTc'; // токен вашего бота
-$data = file_get_contents('php://input'); // весь ввод перенаправляем в $data
-$data = json_decode($data, true); // декодируем json-закодированные-текстовые данные в PHP-массив
+$data = json_decode(file_get_contents('php://input'), TRUE);
+//пишем в файл лог сообщений
+//file_put_contents('file.txt', '$data: '.print_r($data, 1)."\n", FILE_APPEND);
 
-// Для отладки, добавим запись полученных декодированных данных в файл message.txt, 
-// который можно смотреть и понимать, что происходит при запросе к боту
-// Позже, когда все будет работать закомментируйте эту строку:
-file_put_contents(__DIR__ . '/message.txt', print_r($data, true));
+$data = $data['callback_query'] ? $data['callback_query'] : $data['message'];
 
-// Основной код: получаем сообщение, что юзер отправил боту и 
-// заполняем переменные для дальнейшего использования
-if (!empty($data['message']['text'])) {
-    $chat_id = $data['message']['from']['id'];
-    $user_name = $data['message']['from']['username'];
-    $first_name = $data['message']['from']['first_name'];
-    $last_name = $data['message']['from']['last_name'];
-    $text = trim($data['message']['text']);
-    $text_array = explode(" ", $text);
-    
-    if ($text == '/help') {
-        $text_return = "Привет, $first_name $last_name, вот команды, что я понимаю: 
-/help - список команд
-/about - о нас
-";
-        message_to_telegram($bot_token, $chat_id, $text_return);
-    }
-    elseif ($text == '/about') {
-        $text_return = "verysimple_bot:
-Я пример самого простого бота для телеграм, написанного на простом PHP.
-Мой код можно скачивать, дополнять, исправлять. Код доступен в этой статье:
-https://www.novelsite.ru/kak-sozdat-prostogo-bota-dlya-telegram-na-php.html
-";
-        message_to_telegram($bot_token, $chat_id, $text_return);
-    }
+define('TOKEN', '5358046782:AAGiDv_HBq7TcwgWAHFfl_FgHlVJLP0myTc');
 
+$message = mb_strtolower(($data['text'] ? $data['text'] : $data['data']),'utf-8');
+
+
+switch ($message) {
+    case 'да':
+        $method = 'sendMessage';
+		$send_data = [
+			'text' => 'Что вы хотите заказать?',
+			'reply_markup'  => [
+				'resize_keyboard' => true,
+				'keyboard' => [
+						[
+							['text' => 'Яблоки'],
+							['text' => 'Груши'],
+						],
+						[
+							['text' => 'Лук'],
+							['text' => 'Чеснок'],
+						]
+					]
+				]
+			];
+    break;
+	case 'нет':
+        $method = 'sendMessage';
+		$send_data = ['text' => 'Приходите еще'];
+    break;
+	case 'яблоки':
+        $method = 'sendMessage';
+		$send_data = ['text' => 'заказ принят!'];
+    break;
+	case 'груши':
+        $method = 'sendMessage';
+		$send_data = ['text' => 'заказ принят!'];
+    break;
+	case 'лук':
+        $method = 'sendMessage';
+		$send_data = ['text' => 'заказ принят!'];
+    break;
+	case 'чеснок':
+        $method = 'sendMessage';
+		$send_data = ['text' => 'заказ принят!'];
+    break;
+	default:
+		$method = 'sendMessage';
+		$send_data = [
+			'text' => 'Вы хотите сделать заказ?',
+			'reply_markup'  => [
+				'resize_keyboard' => true,
+				'keyboard' => [
+						[
+							['text' => 'Да'],
+							['text' => 'Нет'],
+						]
+					]
+				]
+			];
 }
 
-// функция отправки сообщени в от бота в диалог с юзером
-function message_to_telegram($bot_token, $chat_id, $text, $reply_markup = '')
+$send_data['chat_id'] = $data['chat'] ['id'];
+
+$res = sendTelegram($method, $send_data);
+
+
+
+
+function sendTelegram($method, $data, $headers = [])
 {
-    $ch = curl_init();
-    $ch_post = [
-        CURLOPT_URL => 'https://api.telegram.org/bot' . $bot_token . '/sendMessage',
-        CURLOPT_POST => TRUE,
-        CURLOPT_RETURNTRANSFER => TRUE,
-        CURLOPT_TIMEOUT => 10,
-        CURLOPT_POSTFIELDS => [
-            'chat_id' => $chat_id,
-            'parse_mode' => 'HTML',
-            'text' => $text,
-            'reply_markup' => $reply_markup,
-        ]
-    ];
-
-    curl_setopt_array($ch, $ch_post);
-    curl_exec($ch);
+	$curl = curl_init();
+	curl_setopt_array($curl, [
+		CURLOPT_POST => 1,
+		CURLOPT_HEADER => 0,
+		CURLOPT_RETURNTRANSFER => 1,
+		CURLOPT_URL => 'https://api.telegram.org/bot' . TOKEN . '/' . $method,
+		CURLOPT_POSTFIELDS => json_encode($data),
+		CURLOPT_HTTPHEADER => array_merge(array("Content-Type: application/json"))
+	]);
+	$result = curl_exec($curl);
+	curl_close($curl);
+	return (json_decode($result, 1) ? json_decode($result, 1) : $result);
 }
+
+?>
